48c2382266a19cc767ed83520fec3c68eaf1abfb
otaeyean <oty12125@naver.com>
2023-11-18 13:55:44 +0000

    Third commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 // Hello.java
  public class Hello {
     public static void main(String[] args) {
-          System.out.println("Hello, world from GIT");             }
+          System.out.println("Hello, world from GT");             }
              }
